bc023aad426588d4a7085a9190d671647a22b319
jonnyalexbh <jonnyalex.bh@gmail.com>
2021-06-23 15:23:48 -0500

    Fix errors

--- app/controllers/portfolios.js
+++ app/controllers/portfolios.js
@@ -1,5 +1,5 @@
 const { getPortfolio } = require('../interactors/portfolios');
 
-exports.getPortfolio = async (req, res) => getPortfolio(req.params.id)
+exports.getPortfolio = (req, res) => getPortfolio(req.params.id)
   .then((portfolio) => res.render('portfolio', { req, portfolio }))
   .catch((error) => res.render('error', { req, error: error.message }));
